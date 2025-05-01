0cb7dbcbaa66f44d9299e84cd01a0a958e350781
r2213432 <jhojanmartinezmayta@gmail.com>
2025-05-01 05:51:35 +0200

    Puseando lo de Aitor el insano XD

---
+++ frontend/src/app/pages/pvz-a/pvz-a.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pvz-a',
+  templateUrl: './pvz-a.component.html',
+  styleUrls: ['./pvz-a.component.css']
+})
+export class PvzAComponent {
+  nombrePlanta = 'lanzacancer';
+}
